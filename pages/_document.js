835e6fb47a0924e432de3da49eb8e1831387918a
Marc Fehr <marc.fehr@gmail.com>
2023-03-30 05:56:48 +0200

    fontSIze 62.5% in _document

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html lang='en'>
+      <Html lang='en' style={{fontSize: '62.5%'}}>
         <Head />
         <body>
           <Main />
