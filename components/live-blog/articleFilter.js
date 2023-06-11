95cd5685323694b8e0daa331ac9e04278db3ed80
Marc Fehr <marc.fehr@gmail.com>
2023-06-11 18:55:17 +0200

    make filter links faster

--- components/live-blog/articleFilter.js
+++ components/live-blog/articleFilter.js
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 const ArticleFilter = ({ authors, lang, currentFilter }) => {
 	const filterText = lang === 'en' ? 'Filter by author' : 'Filtrer par auteur'
 	const sortedAuthors = authors.sort((a, b) => a.slug.localeCompare(b.slug))
@@ -7,16 +9,12 @@ const ArticleFilter = ({ authors, lang, currentFilter }) => {
 			<h3 className={'font-sans font-bold text-base'}>{filterText}</h3>
 			{sortedAuthors.map((el) => {
 				if (el.slug === currentFilter) {
-					return (
-						<span className={'block'}>
-							{el.name}
-						</span>
-					)
+					return <span className={'block'}>{el.name}</span>
 				}
 				return (
-					<a href={`/car-live-blog/${lang}/by-author/${el.slug}`} className={'block'}>
-						{el.name}
-					</a>
+					<Link href={`/car-live-blog/${lang}/by-author/${el.slug}`}>
+						<p className={'block cursor-pointer text-burgundy font-medium hover:underline'}>{el.name}</p>
+					</Link>
 				)
 			})}
 		</li>
