ed6397576b7318f834bf9664d420476be9cb101a
Marc Fehr <marc.fehr@gmail.com>
2023-10-02 18:10:30 +0200

    video and audio player are issue

--- components/live-blog/video.js
+++ components/live-blog/video.js
@@ -1,14 +1,14 @@
-import { useEffect, useState } from 'react'
+// import { useEffect, useState } from 'react'
 
 import ReactPlayer from 'react-player/lazy'
 import RichtextComponent from '@components/live-blog/text'
 // import VisibilitySensor from 'react-visibility-sensor'
 
+// const VideoPlayer = ({ el, i, autoPlay, lang }) => {
 const VideoPlayer = ({ el, i, autoPlay, lang }) => {
-	console.log(el)
 
-	if (el.youtubeId) {
-		return (
+	return (
+		<>
 			<>
 				<div className={`bg-black p-3 flex flex-col items-center justify-center ${el.caption?.json ? '' : 'mb-8'}`}>
 					<div className={'relative w-full block h-0 pt-16/9'}>
@@ -42,43 +42,11 @@ const VideoPlayer = ({ el, i, autoPlay, lang }) => {
 					</p>
 				)}
 			</>
-		)
-	}
+		</>
 
-	if (el.video?.url) {
-		return (
-			<>
-				<div className={`bg-black p-3 flex flex-col items-center justify-center ${el.caption?.json ? '' : 'mb-8'}`}>
-					<div className={'relative w-full block h-0 pt-16/9'}>
-						<ReactPlayer
-							width={'100%'}
-							config={{
-								youtube: {
-									playerVars: {
-										cc_lang_pref: lang || 'en',
-										cc_load_policy: 1,
-									},
-								},
-							}}
-							controls
-							playsinline
-							url={`${el.video.url}`}
-						/>
-						{el.credit && (
-							<div className={'absolute left-0 bottom-0 bg-white opacity-80 hover:opacity-100 px-1'}>
-								<span>{el.credit}</span>
-							</div>
-						)}
-					</div>
-				</div>
-				{el.caption?.json && (
-					<p className={'px-1 py-2'}>
-						<RichtextComponent content={el.caption.json} />
-					</p>
-				)}
-			</>
-		)
-	}
+	)
+
+	
 }
 
 export default VideoPlayer
