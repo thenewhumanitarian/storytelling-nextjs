afbf9fe1da3703701935204d0753b79d0333c49d
Marc Fehr <marc.fehr@gmail.com>
2023-10-02 13:32:31 +0200

    styling homepage

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 export default function Index() {
 	return (
 		<div className={'w-full h-full absolute top-0 left-0 flex justify-center items-center'}>
-			<div>
-				<a href={'https://www.thenewhumanitarian.org'}>
+			<div className={'bg-burgundy text-white p-3'}>
+				<a href={'https://www.thenewhumanitarian.org'} className={'text-white'}>
 					<h1>The New Humanitarian</h1>
 				</a>
-				<p>This is an empty page – click on the title above to see our homepage.</p>
+				<p className={'text-white'}>This is an empty page – click on the title above to see our homepage.</p>
 			</div>
 		</div>
 	)
