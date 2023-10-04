a914e29aabe8b55e8a83118427ce3e4d405667e3
Marc Fehr <marc.fehr@gmail.com>
2023-10-04 18:18:21 +0200

    styling sidebar

--- components/live-blog/sidebar.js
+++ components/live-blog/sidebar.js
@@ -5,7 +5,7 @@ import ArticleAuthorInfo from '@components/live-blog/articleAuthorInfo'
 
 const Sidebar = ({ lang, title, liveBlogPages, showFilter, currentFilter, hideBackButton, author, className }) => {
 	return (
-		<div className={`${className || ''} col-span-2 bg-burgundy sm:bg-transparent p-3`}>
+		<div className={`${className || ''} col-span-2 bg-burgundy sm:bg-transparent p-3 sm:p-0`}>
 			{!hideBackButton && (
 				<Link href={`${lang === 'en' ? '/car-live-blog/en' : '/car-live-blog/fr'}`}>
 					<button className={'bg-burgundy px-3 py-2 text-white font-normal text-sm mb-5'}>{lang === 'en' ? '← Back to overview' : '← Retour'}</button>
