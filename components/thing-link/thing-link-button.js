dbaa4c81f0d656351372095ac92f958e846ab772
Marc Fehr <marc.fehr@gmail.com>
2023-10-19 16:55:23 +0200

    smaller text box

--- components/thing-link/thing-link-button.js
+++ components/thing-link/thing-link-button.js
@@ -132,32 +132,33 @@ const ThingLinkButton = ({ data }) => {
             />
             <AnimatePresence>
                 {isOpen && (
-                    <motion.div
-                        onClick={() => setIsOpen(false)}
-                        initial={{
-                            opacity: 0,
-                        }}
-                        animate={{
-                            opacity: 1,
-                        }}
-                        enter={{ opacity: 1 }}
-                        exit={{ opacity: 0 }}
-                        transition={spring}
-                        className={`fixed flex justify-start items-start w-full top-0 left-0 w-full h-full bg-white z-50 fixed border-black border-2 overflow-y-auto`}
-                    >
-                        <motion.div className={'pt-5 pl-5 pb-5 pr-16 overflow-y-auto'}>
-                            <RichtextComponent content={data.text.json} />
-                        </motion.div>
+                    <motion.div exit={{ background: 'transparent'}} animate={{ background: isOpen ? 'rgba(255,255,255,0.5)' : 'transparent' }} className={`flex w-full h-full ${isOpen ? 'bg-white bg-opacity-50' : 'bg-transparent'} flex items-center justify-center`} onClick={() => setIsOpen(false)}>
                         <motion.div
-                            className={'w-8 h-8 block absolute top-5 right-5 text-red-500 cursor-pointer'}
-                            whileHover={{ scale: 1.2 }}
-                            initial={{ rotate: '-40deg' }}
-                            exit={{ rotate: '40deg', opacity: 0 }}
-                            animate={{ rotate: 0 }}
+                            initial={{
+                                opacity: 0,
+                            }}
+                            animate={{
+                                opacity: 1,
+                            }}
+                            enter={{ opacity: 1 }}
+                            exit={{ opacity: 0 }}
                             transition={spring}
-                            onClick={() => setIsOpen(false)}
+                            className={`w-auto max-w-xl h-auto bg-white z-50 fixed border-black border-2 overflow-y-auto`}
                         >
-                            <CloseIcon />
+                            <motion.div className={'pt-5 pl-5 pb-5 pr-16 overflow-y-auto'}>
+                                <RichtextComponent content={data.text.json} />
+                            </motion.div>
+                            <motion.div
+                                className={'w-8 h-8 block absolute top-5 right-5 text-red-500 cursor-pointer'}
+                                whileHover={{ scale: 1.2 }}
+                                initial={{ rotate: '-40deg' }}
+                                exit={{ rotate: '40deg', opacity: 0 }}
+                                animate={{ rotate: 0 }}
+                                transition={spring}
+                                onClick={() => setIsOpen(false)}
+                            >
+                                <CloseIcon />
+                            </motion.div>
                         </motion.div>
                     </motion.div>
                 )}
